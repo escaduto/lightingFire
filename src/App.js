2d1583f863faab6067c5dafaccba6878ceab9277
Erica <erica@Sophals-MacBook-Pro-2.local>
2020-08-24 21:40:04 -0700

    lighting fire progression map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Slider from '@material-ui/core/Slider';
+import Container from '@material-ui/core/Container';
 import './App.css';
+import dayOneFire from './assets/LNU_08182020.jpg';
+import dayTwoFire from './assets/LNU_08192020.jpg';
+import dayThreeFire from './assets/LNU_08202020.jpg';
+import dayFourFire from './assets/LNU_08212020.jpg';
+import dayFiveFire from './assets/LNU_08222020.jpg';
+import daySixFire from './assets/LNU_08232020.jpg';
+
+const marks = [
+  {
+    value: 1,
+    label: '8/18',
+  },
+  {
+    value: 2,
+    label: '8/19',
+  },
+  {
+    value: 3,
+    label: '8/20',
+  },
+  {
+    value: 4,
+    label: '8/21',
+  },
+  {
+    value: 5,
+    label: '8/22',
+  },
+  {
+    value: 6,
+    label: '8/23',
+  },
+];
+
+const dayToImageMap = {
+  1: dayOneFire,
+  2: dayTwoFire,
+  3: dayThreeFire,
+  4: dayFourFire,
+  5: dayFiveFire,
+  6: daySixFire
+}
 
 function App() {
+
+  const [slide, setSlide] = useState(1);
+
+  const valuetext = (value) => {
+    return `${17+value}`;
+  }
+
+  const handleChange = (event, newValue) => {
+    setSlide(newValue);
+  };
+
+  console.log({ slide })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Container maxWidth="md">
+        <Slider
+          onChange={handleChange}
+          defaultValue={1}
+          getAriaValueText={valuetext}
+          aria-labelledby="discrete-slider"
+          valueLabelDisplay="off"
+          step={1}
+          marks={marks}
+          min={1}
+          max={marks.length}
+        />
+        <img className="fireImage" src={dayToImageMap[slide]} alt="" />
+       </Container>
     </div>
   );
 }
